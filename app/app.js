a91f488373bd1e39d61d9e5303e9af591a110cd5
Rudi van Litsenborgh <rudi.vanlitsenborgh@bt.com>
2016-02-09 23:08:27 +0000

    common angular directives added

--- app/app.js
+++ app/app.js
@@ -5,11 +5,19 @@
 	var app = angular.module('showroom', []);
 
 	app.controller('ShowroomController', function(){
-		var car = {
+
+		var cars = [{
 			name: 'Ferrari',
 			price: 115000,
-			year: 1979
-		}
+			year: 1979,
+			description: 'This is an excellent classic that has stood the test of time.',
+			canPurchase: true,
+			soldOut: false
+		}];
+
+		this.cars = cars;
+		console.info(cars);
+
 	});
 	
 })();
