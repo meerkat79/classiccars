e5632fe9fa4d306bb7ccdab7ddf1dc7789eaa57d
Rudi van Litsenborgh <rvlglobe@gmail.com>
2016-02-19 17:23:36 +0000

    fix folder name

--- app/app.js
+++ app/app.js
@@ -216,54 +216,6 @@
 			this.review = {};
 		};
 
-		var cars = [
-		{
-			name: 'Ferrari',
-			price: 155000,
-			year: 1979,
-			description: 'This is an excellent classic that has stood the test of time.',
-			canPurchase: true,
-			soldOut: false,
-			images: [
-				{
-					thumb: '',
-					full: 'assets/img/FERRARI-308-GTSI.jpg'
-				}
-			]
-		},
-		{
-			name: 'Bentley',
-			price: 139000,
-			year: 1967,
-			description: 'This is an excellent classic that has stood the test of time.',
-			canPurchase: true,
-			soldOut: false
-		},
-		{
-			name: 'Jaguar',
-			price: 120000,
-			year: 1964,
-			description: 'This is an excellent classic that has stood the test of time.',
-			canPurchase: true,
-			soldOut: false
-		},
-		{
-			name: 'Mercedes',
-			price: 115000,
-			year: 1958,
-			description: 'This is an excellent classic that has stood the test of time.',
-			canPurchase: true,
-			soldOut: false
-		},
-		{
-			name: 'Porche',
-			price: 124500,
-			year: 1981,
-			description: 'This is an excellent classic that has stood the test of time.',
-			canPurchase: true,
-			soldOut: false
-		}];
-
 		this.cars = cars;
 		console.info(cars);
 	});
